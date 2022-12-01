e00649ad91274f783659569cd55e1f654c4459a3
Leah Cardoz <leahcardoz@gmail.com>
2022-12-01 15:41:06 -0500

    commit tree 978f18da1d1c81ea9c340a1c26ccd581bc46a9d3

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+puts "seeding..."
+
+Show.create(name: "Ted Lasso", network: "Apple TV", day: "Friday", rating: 90, season: "second")
+Show.create(name: "Dark", network: "Netflix", day: "all", rating: 99, season: "third")
+Show.create(name: "The White Lotus", network: "HBO", day: "Sunday", rating: 80, season: "second")
+Show.create(name: "The Crown", network: "Netflix", day: "i dont know", rating: 1, season: "who cares")
+
+puts "done seeding!"
